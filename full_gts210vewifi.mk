82ba946932b386aebb854ba7763990d98e1f2f62
Olivier Karasangabo <olivier@olivierk.eu>
2017-01-22 13:48:12 +0100

    gts21vewifi: Inherit appropriate base config
    * This is a WiFi-only tab; inheriting telephony base isn't necessary.

--- full_gts210vewifi.mk
+++ full_gts210vewifi.mk
@@ -16,7 +16,7 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
 # Inherit from gts210vewifi device
 $(call inherit-product, device/samsung/gts210vewifi/device.mk)
