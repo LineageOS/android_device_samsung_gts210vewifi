744f114c2b7239ee61ea6898fefd747ab5fe207a
Luca Stefani <luca020400@lineageos.org>
2017-08-19 13:17:46 +0200

    gts210vewifi: Update common_full_tablet_wifionly path

--- lineage.mk
+++ lineage.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, device/samsung/gts210vewifi/full_gts210vewifi.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
 
 PRODUCT_NAME := lineage_gts210vewifi
 PRODUCT_BRAND := samsung
