6158f94d62e61a7a1d188c409df7e2e8c6d910db
Bruno Martins <bgcngm@gmail.com>
2018-08-10 13:47:04 +0100

    gts210vewifi: Consider lineage_gts210vewifi product via the AOSP way
    Change-Id: Ibdeb62f0aa30933a09ac12ca825025e5f18574ab

--- lineage_gts210vewifi.mk
+++ lineage_gts210vewifi.mk
@@ -21,9 +21,21 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 # Inherit from gts210vewifi device
 $(call inherit-product, device/samsung/gts210vewifi/device.mk)
 
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := gts210vewifi
-PRODUCT_NAME := full_gts210vewifi
+PRODUCT_NAME := lineage_gts210vewifi
 PRODUCT_BRAND := samsung
-PRODUCT_MODEL := gts210vewifi
+PRODUCT_MODEL := SM-T813
 PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+# Use the latest approved GMS identifiers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=gts210vewifixx \
+    PRIVATE_BUILD_DESC="gts210vewifixx-user 7.0 NRD90M T813XXS2BRC2 release-keys"
+
+BUILD_FINGERPRINT := "samsung/gts210vewifixx/gts210vewifi:7.0/NRD90M/T813XXS2BRC2:user/release-keys"
